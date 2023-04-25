1f7f5a58dc09cdad38ee89450ba51d31ca8bd9ae
Tomas Zuber <tomas.zuber@external.erstegroup.com>
2023-04-25 10:58:00 +0200

    Added new tests

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -44,7 +44,7 @@ class GildedRose(var items: List<Item>) {
                             }
                         }
                     } else {
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                     }
                 } else {
                     if (item.quality < 50) {
--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,7 +6,43 @@ import org.junit.jupiter.api.Test
 internal class GildedRoseTest {
 
     @Test
-    fun foo() {
+    fun `foo quality cannot be negative`() {
+        val items = listOf(Item("foo", 0, 0))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        val item = app.items[0]
+        assertEquals("foo", item.name)
+        assertEquals(0, item.quality)
+    }
+
+    @Test
+    fun `foo with quality 1`() {
+        val items = listOf(Item("foo", 0, 1))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        val item = app.items[0]
+        assertEquals("foo", item.name)
+        assertEquals(0, item.quality)
+    }
+
+    @Test
+    fun `foo with quality 2`() {
+        val items = listOf(Item("foo", 0, 2))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        val item = app.items[0]
+        assertEquals("foo", item.name)
+        assertEquals(0, item.quality)
+    }
+
+    @Test
+    fun `foo sellIn can be negative`() {
         val items = listOf(Item("foo", 0, 0))
         val app = GildedRose(items)
 
@@ -15,9 +51,33 @@ internal class GildedRoseTest {
         val item = app.items[0]
         assertEquals("foo", item.name)
         assertEquals(-1, item.sellIn)
-        assertEquals(0, item.quality)
     }
 
+    @Test
+    fun `Aged Brie with 0 sellIn 0 quality`() {
+        val items = listOf(Item("Aged Brie", 0, 0))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        val item = app.items[0]
+        assertEquals("Aged Brie", item.name)
+        assertEquals(-1, item.sellIn)
+        assertEquals(2, item.quality)
+    }
+
+    @Test
+    fun `Backstage passes to a TAFKAL80ETC concert`() {
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 0, 0))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        val item = app.items[0]
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", item.name)
+        assertEquals(-1, item.sellIn)
+        assertEquals(0, item.quality)
+    }
 }
 
 
